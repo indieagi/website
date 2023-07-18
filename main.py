29b8bd816265c1ac419b8f57c8e545e48b43d1e1
Aaditya Rajesh <aadityalr123@gmail.com>
2023-07-18 14:03:35 -0700

    Route and HTML to /events/intro-to-full-stack-llm

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/events/intro-to-full-stack-llm')
+def intro_to_full_stack_llm():
+    return render_template('intro_to_full_stack_llm.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
