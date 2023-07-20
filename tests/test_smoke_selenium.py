4940aea8085077759c1d427a6d58672c750e992e
Aaditya Rajesh <aadityalr123@gmail.com>
2023-07-19 20:37:02 -0700

    enhanced readability

--- tests/test_smoke_selenium.py
+++ tests/test_smoke_selenium.py
@@ -8,61 +8,72 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-class TestWebPages(unittest.TestCase):
+class WebPageTest(unittest.TestCase):
     def setUp(self):
+        self.initialize_web_driver()
+
+    def tearDown(self):
+        self.quit_web_driver()
+
+    def test_web_pages(self):
+        urls_to_test = self.urls_to_test()
+        self.assert_urls_load_correctly(urls_to_test)
+
+    def initialize_web_driver(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
 
         try:
-            # download chromedriver from https://chromedriver.chromium.org/downloads. Python will automatically find it in your PATH.
             self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
         except WebDriverException as e:
-            self.fail(f'WebDriver setup failed, error is: {str(e)}')
+            self.fail(f'WebDriver setup failed: {str(e)}')
 
-        # Implicitly wait for a certain duration while trying to find any element or elements
         self.driver.implicitly_wait(10)  # seconds
 
-    def tearDown(self):
+    def quit_web_driver(self):
         self.driver.quit()
 
-    def generate_test_urls(self):
-        """
-        Generate a list of test URLs from the index.html file in the templates directory,
-        including only links starting with https://www.indieagi.org.
-        """
-        test_urls = ["https://www.indieagi.org"]
-        filename = "./templates/index.html"
+    def urls_to_test(self):
+        return self.get_urls_from_index_html() + ["https://www.indieagi.org"]
 
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                contents = f.read()
+    def get_urls_from_index_html(self):
+        urls = []
+        index_html_path = "./templates/index.html"
 
-            soup = BeautifulSoup(contents, 'html.parser')
+        if os.path.exists(index_html_path):
+            with open(index_html_path, 'r') as f:
+                html_content = f.read()
+
+            soup = BeautifulSoup(html_content, 'html.parser')
 
             for link in soup.find_all('a'):
                 url = link.get('href')
                 if url.startswith("https://www.indieagi.org"):
-                    test_urls.append(url)
+                    urls.append(url)
+    
+        return urls
 
-        return test_urls
+    def assert_urls_load_correctly(self, urls):
+        for url in urls:
+            self.assert_url_loads_correctly(url)
 
-    def test_webpage_load(self):
-        test_urls = self.generate_test_urls()
+    def assert_url_loads_correctly(self, url):
+        self.assert_url_resolves(url)
+        self.assert_url_returns_ok_status(url)
 
-        for url in test_urls:
-            try:
-                response = requests.get(url)
-            except requests.exceptions.RequestException as e:
-                print(f'DNS resolution failed for {url}, error is: {str(e)}')
-                self.fail(f"DNS resolution failed for {url}")
+    def assert_url_resolves(self, url):
+        try:
+            requests.get(url)
+        except requests.exceptions.RequestException as e:
+            self.fail(f"DNS resolution failed for {url}: {str(e)}")
 
-            try:
-                self.driver.get(url)
-                self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code} for the URL {url}")
-            except WebDriverException as e:
-                self.fail(f"DNS resolution succeeded but failed to get 200 OK for {url}. Error: {str(e)}")
-
+    def assert_url_returns_ok_status(self, url):
+        try:
+            self.driver.get(url)
+            response = requests.get(url)
+            self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code} for the URL {url}")
+        except WebDriverException as e:
+            self.fail(f"DNS resolution succeeded but failed to get 200 OK for {url}. Error: {str(e)}")
 
 if __name__ == "__main__":
     unittest.main()
