44b7dea1a86bd536ffed61a7c19447946ab3a4d4
Toby Tobkin <toby@valuestream.ai>
2023-07-20 13:47:37 -0700

    rewrote smoke test

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
----
-title: Flask
-description: A popular minimal server framework for Python
-tags:
-  - python
-  - flask
----
-
-# Python Flask Example
-
-This is a [Flask](https://flask.palletsprojects.com/en/1.1.x/) app that serves a simple JSON response.
-
-[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/zUcpux)
-
-## ✨ Features
-
-- Python
-- Flask
-
-## 💁‍♀️ How to use
-
+# Setup
+## System Setup
+### Ubuntu
+- Download the latest version of ChromeDriver
+- Run the following:
+```
+unzip chromedriver_linux64.zip
+cd chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin
+sudo chmod +x /usr/bin/chromedriver
+```
+- Test the installation
+```
+chromedriver --port=4444
+# Navigate to http://localhost:4444 in your browser
+# Success is if you see something like the following:
+{"value":{"error":"unknown command","message":"unknown command: unknown command: ","stacktrace":"#0 0x5643a3570783 \u003Cunknown>\n#1 0x5643a329c917 \u003Cunknown>\n#2 0x5643a32f78de \u003Cunknown>\n#3 0x5643a32f75f5 \u003Cunknown>\n#4 0x5643a3269da3 \u003Cunknown>\n#5 0x5643a3531e98 \u003Cunknown>\n#6 0x5643a3535d67 \u003Cunknown>\n#7 0x5643a354032c \u003Cunknown>\n#8 0x5643a3536993 \u003Cunknown>\n#9 0x5643a3505827 \u003Cunknown>\n#10 0x5643a32682d1 \u003Cunknown>\n#11 0x7f5520e23a90 \u003Cunknown>\n"}}
+```
+## Python Setup
+- If you haven't aleady, create your venv `create_venv.sh`
+- Activate your venv `source dev_venv/bin/activate`
 - Install Python requirements `pip install -r requirements.txt`
-- Start the server for development `python3 main.py`
+# How to run
+- Start the server for development `python main.py`
+- Navigate to `127.0.0.1:5000`
+
+# How to test
+- `python tests/test_smoke_selenium.py`
--- tests/test_smoke_selenium.py
+++ tests/test_smoke_selenium.py
@@ -1,79 +1,21 @@
-import os
-import unittest
-import requests
-from bs4 import BeautifulSoup
+import subprocess
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-class WebPageTest(unittest.TestCase):
-    def setUp(self):
-        self.initialize_web_driver()
+def test_smoke_selenium():
+  try:
+    server = subprocess.Popen(["python", "main.py"], stdout=subprocess.PIPE)
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:5000")
 
-    def tearDown(self):
-        self.quit_web_driver()
+    assert "Weekly Lightning Talk Meetup" in driver.page_source, """Test Failed: 
+    Text not found: Weekly Lightning Talk Meetup"""
 
-    def test_web_pages(self):
-        urls_to_test = self.urls_to_test()
-        self.assert_urls_load_correctly(urls_to_test)
+  except WebDriverException as e:
+    print(f"Test Failed: WebDriverException encountered. {str(e)}")
 
-    def initialize_web_driver(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
+  finally:
+    driver.quit()
+    server.terminate()
 
-        try:
-            self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-        except WebDriverException as e:
-            self.fail(f'WebDriver setup failed: {str(e)}')
-
-        self.driver.implicitly_wait(10)  # seconds
-
-    def quit_web_driver(self):
-        self.driver.quit()
-
-    def urls_to_test(self):
-        return self.get_urls_from_index_html() + ["https://www.indieagi.org"]
-
-    def get_urls_from_index_html(self):
-        urls = []
-        index_html_path = "./templates/index.html"
-
-        if os.path.exists(index_html_path):
-            with open(index_html_path, 'r') as f:
-                html_content = f.read()
-
-            soup = BeautifulSoup(html_content, 'html.parser')
-
-            for link in soup.find_all('a'):
-                url = link.get('href')
-                if url.startswith("https://www.indieagi.org"):
-                    urls.append(url)
-    
-        return urls
-
-    def assert_urls_load_correctly(self, urls):
-        for url in urls:
-            self.assert_url_loads_correctly(url)
-
-    def assert_url_loads_correctly(self, url):
-        self.assert_url_resolves(url)
-        self.assert_url_returns_ok_status(url)
-
-    def assert_url_resolves(self, url):
-        try:
-            requests.get(url)
-        except requests.exceptions.RequestException as e:
-            self.fail(f"DNS resolution failed for {url}: {str(e)}")
-
-    def assert_url_returns_ok_status(self, url):
-        try:
-            self.driver.get(url)
-            response = requests.get(url)
-            self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code} for the URL {url}")
-        except WebDriverException as e:
-            self.fail(f"DNS resolution succeeded but failed to get 200 OK for {url}. Error: {str(e)}")
-
-if __name__ == "__main__":
-    unittest.main()
+test_smoke_selenium()
